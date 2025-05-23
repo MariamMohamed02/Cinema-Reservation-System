599e6f9af0e76b0bfc95d56056fdd7467f4248eb
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-05-24 01:30:42 +0300

    Profile UI Done

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-profile',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './profile.component.html',
-//   styleUrl: './profile.component.css'
-// })
-// export class ProfileComponent {
-
-// }
 
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
